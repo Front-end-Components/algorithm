747619aff0f611b043c3b77f91e9d313e6d942ad
jisooround <9utesleben@gmail.com>
2023-01-26 23:27:53 +0900

    폴더 이동

--- 2022-12-09/과일장수/박정민/과일_박정민.js
+++
@@ -1,9 +0,0 @@
-function solution(k, m, score) {
-  let result = 0;
-  score = score.sort((a, b) => a - b).slice(score.length % m);
-  
-  for(let i = 0; i < score.length; i += m) {
-      result += score[i] * m
-  }
-  return result;
-}
--- 2022-12-16/옹알이(2)/박정민/옹알이_박정민.js
+++
@@ -1,8 +0,0 @@
-function solution(babbling) {
-  let result = 0;
-  for(let i = 0; i < babbling.length; i++) {
-        if(/(aya|ye|woo|ma)\1+/g.test(babbling[i])) continue;
-        if(/^(aya|ye|woo|ma)+$/g.test(babbling[i])) result++;
-    }
-  return result;
-}

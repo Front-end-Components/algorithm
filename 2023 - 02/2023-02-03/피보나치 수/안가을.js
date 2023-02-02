57723e445661e0549d29ba3fc9ab019535a0542b
autumnly1007 <autumnly1007@gmail.com>
2023-02-02 19:04:05 +0900

    2023-02-03 안가을

--- 2023 - 02/2023-02-03/피보나치 수/안가을.js
+++ 2023 - 02/2023-02-03/피보나치 수/안가을.js
@@ -5,7 +5,7 @@ function solution(n) {
   // 2부터 n까지 1씩 증가하면서 반복함
   for (let i = 2; i <= n; i++) {
     // stack 배열의 0번째 요소와 1번째 요소를 더하여(피보나치 수) 1234567로 나눈 나머지 값을 해당 배열에 넣음
-    stack.push((stack[0] % 1234567) + (stack[1] % 1234567));
+    stack.push((stack[0] + stack[1]) % 1234567);
 
     // stack 배열의 제일 첫번째 요소를 삭제함 (피보나치 수는 두 개의 숫자만 더하면 되기 때문)
     stack.shift();
